0d087572075e6bcfcbb6e1d25edfe94d3edf3342
eka-martin <99816833+eka-martin@users.noreply.github.com>
2022-09-15 19:47:17 +0200

    start react

--- frontend/groupomania/src/App.js
+++ frontend/groupomania/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+//https://www.section.io/engineering-education/how-to-implement-material-ui-in-react/
 import './App.css';
+import {Button, TextField} from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>Hello world!!!</h1>
+        <Button variant="contained">Contained</Button>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+<TextField id="filled-basic" label="Filled" variant="filled" />
+<TextField id="standard-basic" label="Standard" variant="standard" />
       </header>
     </div>
   );
