91674a009a238ad457196466fa1328ad4f48861c
eka-martin <99816833+eka-martin@users.noreply.github.com>
2022-09-21 12:25:26 +0200

    components

--- frontend/groupomania/src/App.js
+++ frontend/groupomania/src/App.js
@@ -2,19 +2,46 @@
 import './App.css';
 import {Button, TextField} from '@mui/material';
 
+import Counter from './components/Counter';
+
+let n = 0;
+
 function App() {
+  
+  
+  
+  const items = [
+    'Tache1',
+    'Tache2',
+    'Tache3',
+  ]
+  const lis = items.map((item, k) => <li key={k}>{item}</li>)
+
   return (
     <div className="App">
       <header className="App-header">
-        
-        <h1>Hello world!!!</h1>
+        <Counter/>
+       <ul>
+        {lis}
+       </ul>
+        <span>{n}</span>
         <Button variant="contained">Contained</Button>
         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
 <TextField id="filled-basic" label="Filled" variant="filled" />
 <TextField id="standard-basic" label="Standard" variant="standard" />
       </header>
     </div>
+   
+  
   );
+
 }
 
+
+
 export default App;
+
+window.setInterval(() => {
+  n++
+  App()
+}, 100)
