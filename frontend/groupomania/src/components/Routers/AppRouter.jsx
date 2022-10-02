21b0def46a6e8ba21241b9e7d262e5a70da2263f
eka-martin <99816833+eka-martin@users.noreply.github.com>
2022-10-02 19:51:43 +0200

    PostIdPage

--- frontend/groupomania/src/components/Routers/AppRouter.jsx
+++ frontend/groupomania/src/components/Routers/AppRouter.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import {BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import About from "../../pages/About";
 import Posts  from "../../pages/Posts";
 import Error  from "../../pages/Error";
 import PostIdPage from "../../pages/PostIdPage";
+import { routes } from "../../router";
 
 function AppRouter() {
    
   return (
     
         <Routes>
-      <Route path="/about" element={<About/>}/>           
-      <Route exact path="/posts" element={<Posts/>}/>
-      <Route exact path="/posts/:id" element={<PostIdPage/>}/>
-      <Route path="*" element={<Error/>}/>
+      {routes.map((route, index) => {
+        return (
+        <Route
+        element={<route.element/>}
+        path={route.path}
+        key={index}
+        />
+  )
+})
+}
+      <Route path="*" element={<Navigate to="/about" replace />} />
 
 
       </Routes>
--- frontend/groupomania/src/pages/PostIdPage.jsx
+++ frontend/groupomania/src/pages/PostIdPage.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {useParams} from 'react-router-dom';
+import PostService from "../api/PostService";
+import { useFetching } from "../hooks/useFetching";
 
 
 
-function PostIdPage() {
-   
+
+const PostIdPage = () => {
+    const params = useParams()
+    const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
+    const [fetchPostById, isLoading, error] = useFetching( async (id) => {
+    const response = await PostService.getById(id)
+    setPost(response.data);
+       })
+    const [fetchComments, isComLoading, comError] = useFetching( async (id) => {
+    const response = await PostService.getCommentsByPostId(id)
+    setComments(response.data);
+       })
+
+
+  useEffect(() => {
+  fetchPostById(params.id)
+  fetchComments(params.id)
+}, [])
+
   return (
     <div>
-    <h1>
-        You opened a post
-    </h1>
+    <h1>You opened a post with ID = {params.id}</h1>
+    {isLoading
+    ? <h1>Loading.......</h1>
+    : <div>{post.title}</div>
+    }
+    <h1>Comments</h1>
+    {isComLoading
+    ? <h1>Loading.......</h1>
+    : <div>{comments.map(comm =>
+    <div><h5>{comm.email}</h5>
+    <div>{comm.body}</div>
+    </div>
+    )}
+    </div>
+    }
     </div>
   
   );
